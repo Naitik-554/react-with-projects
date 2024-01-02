ff43b870c0091410517f40a42d2d81aea599f506
Naitik-554 <naitikp.official@gmail.com>
2024-01-02 21:37:02 +0530

    progress update

--- my-blog-megaProject/src/App.jsx
+++ my-blog-megaProject/src/App.jsx
@@ -1,10 +1,41 @@
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import authService from './appwrite/auth'
+import { Footer, Header } from './components'
+import { login, logout } from './store/authSlice'
 
 function App() {
-  return (
-    <>
-    <h1>My Blog App with Appwrite</h1>
-    </>
+
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=> {
+      if(userData) {
+        dispatch(login({userData}))
+      }
+      else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => {setLoading(false)})
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen bg-gray-900 flex flex-wrap content-between'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : (
+    <div>Loading...</div>
   )
 }
 
