7010b72c65e0e2ff0276773fd1fd83b572267d4d
Naitik-554 <naitikp.official@gmail.com>
2024-01-04 19:23:38 +0530

    logo added

--- my-blog-megaProject/src/components/Logo.jsx
+++ my-blog-megaProject/src/components/Logo.jsx
@@ -1,7 +1,10 @@
+import logo from '../assets/logo1.png'
 
 function Logo({width = '100px' }) {
   return (
-    <div>Logo</div>
+    <div className="w-20">
+      <img src={logo} alt="Logo"/>
+    </div>
   )
 }
 
